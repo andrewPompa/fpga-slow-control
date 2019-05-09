c69eb639227c2f0fd7084ddb5b7bbfacd41b8583
Michał Jazowski <mjazowski00gmail.com>
2019-05-09 22:09:30 +0200

    [BRAM] Dodałem czyszczenie pamięci

--- bram/bram.c
+++ bram/bram.c
@@ -47,13 +47,9 @@ int main(int argc, char *argv[]) {
     if (program_mode_value.mode == READ) {
         read_from_bram(bram_address_offset, bram_highest_address, program_mode_value.offset, program_mode_value.size);
     } else if (program_mode_value.mode == WRITE) {
-        printf("writing ");
-        print_bytes_as_hex(program_mode_value.value, program_mode_value.value_size);
-        printf(", to address: 0x%lX\n", bram_address_offset);
-
         write_to_bram(bram_address_offset, bram_highest_address, program_mode_value.value, program_mode_value.value_size, program_mode_value.offset);
     } else if (program_mode_value.mode == CLEAR) {
-//        write_to_bram(bram_address_offset, bram_highest_address, 0x0, 0);
+        clear_bram(bram_address_offset, bram_highest_address);
     }
 }
 void read_from_bram(long start_address, long max_address, long offset_bytes, long word_size) {
@@ -70,10 +66,12 @@ void read_from_bram(long start_address, long max_address, long offset_bytes, lon
 
     char *word_bytes = malloc(word_size);
     for (int i = 0; i < word_size; ++i) {
-        printf("[%d] = 0x%X\n", i, bram[i]);
-        word_bytes[i] = bram[i];
+        printf("[0x%lX] = 0x%X\n", start_address + offset_bytes * 8 + i * 8, bram[0]);
+        word_bytes[i] = bram[0];
+        bram++;
     }
     print_bytes_as_hex(word_bytes, word_size);
+    printf("\n");
     close(fd);
 }
 
@@ -98,4 +96,20 @@ void write_to_bram(long start_address, long max_address, char *bytes, long bytes
         bram[i] = bytes[i];
     }
     close(fd);
+}
+
+void clear_bram(long start_address, long max_address) {
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    char *bram;
+    if (fd == -1) {
+        printf("ERROR: cannot open BRAM block...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    signed int bram_size = max_address - start_address;
+    bram = (char *) mmap(NULL, bram_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, start_address );
+    for (int i = 0; i < bram_size >> 3; ++i) {
+        bram[i] = 0;
+    }
+
 }
