7b41eeb6b59798ece0c62e7bb53ef9f702831d1a
Michał Jazowski <mjazowski00gmail.com>
2019-07-11 17:53:12 +0200

    [REST-API] Dodałem obsługę CORSa

--- rest-api/bram.py
+++ rest-api/bram.py
@@ -1,9 +1,23 @@
 import falcon
+from falcon.http_status import HTTPStatus
 
 from bram_resource import BramResource
 
 page_path = 'path-to-page'
-api = application = falcon.API()
+
+
+class HandleCORS(object):
+
+    def process_request(self, req, resp):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', 'GET,HEAD,POST,PATCH,OPTIONS')
+        resp.set_header('Access-Control-Allow-Headers', 'content-type')
+        resp.set_header('Access-Control-Max-Age', 1728000)  # 20 days
+        if req.method == 'OPTIONS':
+            raise HTTPStatus(falcon.HTTP_200, body='\n')
+
+
+api = application = falcon.API(middleware=[HandleCORS()])
 
 bram = BramResource()
 api.add_route('/address/{address}', bram)
