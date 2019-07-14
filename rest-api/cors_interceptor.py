03bdb1b099bc04fb5a701f6dd467d1333426c121
Michał Jazowski <mjazowski00gmail.com>
2019-07-14 20:03:15 +0200

    [REST-API] Dodałem PUT do metod dostępnych przez CORSa

--- rest-api/cors_interceptor.py
+++ rest-api/cors_interceptor.py
@@ -5,7 +5,7 @@ from falcon.http_status import HTTPStatus
 class CORSInterceptor(object):
     def process_request(self, req, resp):
         resp.set_header('Access-Control-Allow-Origin', '*')
-        resp.set_header('Access-Control-Allow-Methods', 'GET,HEAD,POST,PATCH,OPTIONS')
+        resp.set_header('Access-Control-Allow-Methods', 'GET,HEAD,POST,PATCH,OPTIONS,PUT')
         resp.set_header('Access-Control-Allow-Headers', 'content-type')
         resp.set_header('Access-Control-Max-Age', 1728000)  # 20 days
         if req.method == 'OPTIONS':
