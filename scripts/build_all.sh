900da2f3ba0b40bbb22abb1f5e237ae238d98909
Michał Jazowski <mjazowski00gmail.com>
2019-07-16 07:14:49 +0200

    [REST-API] Dodałem skrypt do budowania RESTa na FPGA

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-cp -R /run/media/mmcblk0p2/python-falcon/* ./ &&
+mkdir ./app
+cp -R /run/media/mmcblk0p2/app/* ./app/
+cd ./app/python-libs/
 cd setuptools-41.0.1/ &&
 python setup.py install &&
 cd ../gunicorn-19.9.0/ &&
 python setup.py install &&
 cd ../falcon-2.0.0/ &&
 python setup.py install &&
-cd .. &&
-gunicorn -b 172.168.1.200:8000 --reload bram:application
+cd ../..
+gunicorn -b 172.168.1.201:8000 --reload api_server:application
